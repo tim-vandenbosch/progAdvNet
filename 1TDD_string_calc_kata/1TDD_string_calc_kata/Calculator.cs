884af85f8bc12ec6fc4c5d7b7dc3e08cae8e3d1c
Tim Van Den Bosch <tim_vandenbosch@msn.com>
2017-02-17 23:43:45 +0100

    Edit Calculator
    Added the functionality to add a different delimiter

--- 1TDD_string_calc_kata/1TDD_string_calc_kata/Calculator.cs
+++ 1TDD_string_calc_kata/1TDD_string_calc_kata/Calculator.cs
@@ -13,12 +13,14 @@ namespace _1TDD_string_calc_kata
         private int _indexOfTheNumbersArray = 0;
         private string _rawInputNumbersString;
 
-        public int Add(string input)
+        public int Add(string rawInputNumberString)
         {
-            _rawInputNumbersString = input;
+            _rawInputNumbersString = rawInputNumberString;
+
             if (string.IsNullOrEmpty(_rawInputNumbersString) || _rawInputNumbersString.Equals(" "))
                 return _response = 0;
-            
+
+            CheckForANewDelimiter();
             CheckForNewLinesAndRemoveThem();
             _numbersStringArray = _rawInputNumbersString.Split(_splitter);
             _numbersArray = new int[_numbersStringArray.Length];
@@ -27,15 +29,25 @@ namespace _1TDD_string_calc_kata
             return _response;
         }
 
+        private void CheckForANewDelimiter()
+        {
+            if (!_rawInputNumbersString.Contains("//")) return;
+            _splitter = Convert.ToChar(_rawInputNumbersString.Substring(2, 1));
+            _rawInputNumbersString = _rawInputNumbersString.Substring(3);
+        }
+
         private void CheckForNewLinesAndRemoveThem()
         {
             if (_rawInputNumbersString.Contains("\n" + _splitter) || _rawInputNumbersString.Contains(_splitter + "\n"))
             {
-                Console.WriteLine("The given input is incorrect: " + _rawInputNumbersString);
+                Console.WriteLine("The given rawInputNumberString is incorrect: " + _rawInputNumbersString);
                 throw new Exception();
             }
             else
             {
+                if (_rawInputNumbersString.StartsWith("\n"))
+                    _rawInputNumbersString = _rawInputNumbersString.TrimStart('\n');
+
                 if (_rawInputNumbersString.Contains("\n"))
                 {
                     _rawInputNumbersString = _rawInputNumbersString.Replace("\n", _splitter.ToString());
