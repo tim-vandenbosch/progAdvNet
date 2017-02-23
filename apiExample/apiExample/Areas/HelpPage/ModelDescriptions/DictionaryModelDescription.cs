2ef7e531ca7ef6d21af22981482886ee0e6ab487
Tim Van Den Bosch <tim_vandenbosch@msn.com>
2017-02-23 11:05:02 +0100

    init apiExample

---
+++ apiExample/apiExample/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace apiExample.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ apiExample/apiExample/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace apiExample.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
